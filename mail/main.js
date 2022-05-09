4017d88ee20ce15db082e2d50c2fd24b396a152e
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-09 15:56:33 +0200

    mail

--- mail/main.js
+++ mail/main.js
@@ -1,16 +1,20 @@
-const input = document.getElementById('input');
+
 const accesso = document.getElementById('accesso');
 
 const registrazione = document.getElementById('registrazione');
 const button = document.getElementById('button');
 // creare una lista di persone che possono accedere
 const myList = ["giuseppe@gmail.com", "paolo@gmail.com", "simone@hotmail.it", "giovanni@libero.it", "manuele@gmail.com"];
+let input = document.getElementById('input').value = myList;
 // controllare se l'utente è nella lista
 button.addEventListener('click',
 
     function() {
-        
-        if (input = myList) {
+        for (let index = 0; index < myList.length; index++) {
+            let controllo = myList[index];
+            
+        }
+        if (input = controllo) {
             accesso.style.display = 'block';
         }
     
